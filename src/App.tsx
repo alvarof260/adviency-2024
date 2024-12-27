12d1d5e5fea4ccf60bc9fc5266e9868fd21a63e6
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-12-27 20:24:09 +0000

    feat: boton para borrar todos los regalos

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,10 @@ function App() {
     setGifts(newState);
   }
 
+  const deleteAllGift = () => {
+    setGifts([])
+  }
+
   return (
     <Hero>
       <GiftForm addGift={addGift} />
@@ -24,6 +28,19 @@ function App() {
           <GiftItem gift={gift} key={gift.id} deleteGift={deleteGift} />
         ))}
       </GiftList >
+
+      <button
+        className="group relative inline-block text-sm font-medium text-black focus:outline-none focus:ring"
+        onClick={deleteAllGift}
+      >
+        <span className="absolute inset-0 border border-slate-200 group-active:border-slate-200"></span>
+        <span
+          className="block border border-slate-200 bg-slate-200 px-12 py-3 transition-transform active:border-red-100 active:bg-slate-100 group-hover:-translate-x-1 group-hover:-translate-y-1"
+        >
+          Borrar Todo
+        </span>
+      </button>
+
     </Hero>
   )
 }
