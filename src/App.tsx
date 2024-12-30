cb0fd96f64c71bad7a12b8f9ee751d9f17c9228e
Alvaro Estanislao Figueroa <alvarof260@gmail.com>
2024-12-30 00:53:12 +0000

    feat: guardar el listado con localeStorage

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,31 @@ import { GiftForm, GiftList, GiftItem, Hero, NoGift, DeleteAllBtn } from "./comp
 import { Gift } from "./types"
 
 function App() {
-  const [gifts, setGifts] = useState<Gift[]>([]);
+  const [gifts, setGifts] = useState<Gift[]>(() => {
+    const giftsWithLocalStorage = window.localStorage.getItem("gifts");
+    return giftsWithLocalStorage
+      ? JSON.parse(giftsWithLocalStorage)
+      : []
+  });
 
   const addGift = (name: string, quantity: number) => {
     const gift = { id: gifts.length === 0 ? 1 : gifts[gifts.length - 1].id + 1, name, quantity };
     if (!gifts.find(g => g.name === gift.name)) {
       const newState = [...gifts, gift];
       setGifts(newState);
-      console.log(gifts)
+      window.localStorage.setItem("gifts", JSON.stringify(newState));
     }
   }
 
   const deleteGift = (id: number) => {
     const newState = gifts.filter((gift) => gift.id != id);
     setGifts(newState);
+    window.localStorage.setItem("gifts", JSON.stringify(newState));
   }
 
   const deleteAllGift = () => {
     setGifts([]);
+    window.localStorage.removeItem("gifts");
   }
 
   return (
